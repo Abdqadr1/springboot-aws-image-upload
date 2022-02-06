34ddabc41e0a97fc3db1d5c7d857fd8d87d02ab4
Abdqadr1 <abolarinwaquadri@gmail.com>
2022-02-06 18:56:20 +0800

    add user form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import React from 'react';
 import {UserProfiles} from "./components/UserProfiles"
+import AddUser from './components/addUser';
 
 function App() {
   return (
     <div className="App">
+      <AddUser />
       <UserProfiles />
     </div>
   );
